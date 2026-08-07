1550ccb6937e2459672692cd074979a83ce37cbc
freefire <aarbbs@88.com>
2026-08-08 02:11:10 +0800

    Revert "feat: 实现基础滚动位置恢复功能"
    This reverts commit 4042395e3e1775534dd8a6eaa301264e9eedc4bd.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { applyTheme, getStoredTheme } from './utils/theme';
-import { initScrollRestore } from './utils/scrollRestore';
 import App from './App';
 
 applyTheme(getStoredTheme());
-initScrollRestore();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
